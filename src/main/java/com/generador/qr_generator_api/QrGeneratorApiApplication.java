eeceaeef6d380d3880401bf1f7d42ade3253afd1
jahzeelrodas5811 <jahzeelrodasdev@gmail.com>
2025-10-07 09:55:57 -0400

    Configuracion inicial del proyecto con Spring Boot y dependencia de Spring Web

---
+++ src/main/java/com/generador/qr_generator_api/QrGeneratorApiApplication.java
@@ -0,0 +1,13 @@
+package com.generador.qr_generator_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrGeneratorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrGeneratorApiApplication.class, args);
+	}
+
+}
