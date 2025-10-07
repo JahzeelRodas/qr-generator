82fd6b1d08d9251b7bd14c8379bd83d8d52b55ac
jahzeelrodas5811 <jahzeelrodasdev@gmail.com>
2025-10-07 10:13:21 -0400

    Implementar la lógica central de generación de códigos QR usando Zxing en QRCodeService

---
+++ src/main/java/com/generador/qr_generator_api/service/QRCodeService.java
@@ -0,0 +1,23 @@
+package com.generador.qr_generator_api.service;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import org.springframework.stereotype.Service;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+@Service
+public class QRCodeService {
+    public  byte[] generateQRCodeImage(String content, int width, int height)throws WriterException, IOException{
+        QRCodeWriter codeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = codeWriter.encode(content, BarcodeFormat.QR_CODE, width, height);
+        ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
+        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
+        return pngOutputStream.toByteArray();
+    }
+}
